588c1a3836e1924d20fa898f46f0007653c53b55
Gabriel Queiroz <142359384+QsGabriel@users.noreply.github.com>
2025-11-07 15:59:10 -0300

    Adicionando logo ao header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 bg-gradient-to-r from-blue-600 to-blue-700 shadow-lg">
       <div className="max-w-6xl mx-auto px-4 py-2 flex items-center justify-between">
         <img
-          src="/src/public/logo-horizontal.png"
+          src="/logo-horizontal.png"
           alt="Frigoclima Logo"
           className="h-24 object-contain"
         />
