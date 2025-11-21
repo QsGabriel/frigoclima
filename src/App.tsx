e90f72b1c529725058c01230580e74f94fbd924e
Gabriel Queiroz <142359384+QsGabriel@users.noreply.github.com>
2025-11-21 16:41:56 -0300

    desativando o site

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import Features from './components/Features';
-import CTA from './components/CTA';
-import Footer from './components/Footer';
-
 function App() {
   return (
-    <div className="min-h-screen">
-      <Header />
-      <Hero />
-      <About />
-      <Services />
-      <Features />
-      <CTA />
-      <Footer />
+    <div className="min-h-screen w-full flex items-center justify-center bg-black">
+      <img 
+        src="/SITE-SITE-DESATIVADO.png" 
+        alt="Site em Manutenção" 
+        className="w-full h-full object-contain"
+      />
     </div>
   );
 }
